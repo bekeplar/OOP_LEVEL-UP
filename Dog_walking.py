f9ef07e3bbac3d2bad9dc1d0e030d9d4fe4d6d60
bekeplar <bekeplar@gmail.com>
2018-09-23 23:36:10 -0700

    ft(dog-pet-walking):Walking dog [Delivers-#160710393]

--- Dog_walking.py
+++ Dog_walking.py
@@ -1,46 +1,58 @@
 """Parent class"""
 class Pets:
     
+
     dogs = []
 
 
     def __init__(self,dogs):
         self.dogs = dogs
 
+    def walk(self):
+        for dog in self.dogs:
+            print(dog.walk())
+    
 
-"""Another parent class separate from pet"""
+
+"""Another parent class"""
 class Dog:
 
     """Class attribute"""
     species = "mammal"
+    is_hungry = True
 
     """Initialising instance attributes"""
     def __init__(self,name,age):
         self.name = name
         self.age = age
-        self.hungry = True
-
+        self.is_hungry = True
 
     """Instance method"""
     def eat(self):
         self.is_hungry = False
 
-    are_dogs_hungry = False
-    for dog in pets.dogs:
-        if dog.is_hungry:
-            are_dogs_hungry = True
 
-    if are_dogs_hungry:
-        print("My dogs are hungry.")
-        else:
-           print("My dogs are not hungry.")   
+    
 
+    def walk(self):
+        return "%s is walking!" % (self.name)
 
-
+    
     """Create instances of the dogs"""
-
 dogs = [
     Dog("Tom", 6), 
     Dog("Fletcher", 7), 
     Dog("Larry", 9)
 ]
+
+are_dogs_hungry = False
+for dog in pets.dogs:
+    if dog.is_hungry:
+        are_dogs_hungry = True
+
+    if are_dogs_hungry:
+        print("My dogs are hungry.")
+        else:
+           print("My dogs are not hungry.") 
+
+my_pets.walk()         
